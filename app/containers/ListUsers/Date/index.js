9dbcbe5bf7c6b6fdeaf65ab4365abb6d8922d12f
bryan villamil <bryan.v@sancrisoft.com>
2018-10-08 16:57:18 -0500

    changes in the dateComponents

--- app/containers/ListUsers/Date/index.js
+++ app/containers/ListUsers/Date/index.js
@@ -12,10 +12,45 @@ import moment from 'moment';
 
 /* eslint-disable react/prefer-stateless-function */
 class Date extends React.Component {
+  renderSwitch(formatDate) {
+    switch (formatDate) {
+      case 'DayHour':
+        return (
+          // mm-dd-yyyy hh:ss
+          'MM-DD-YYYY hh:ss'
+        );
+      case 'Day':
+        return (
+          // dd mmm yyyy
+          'DD MMM YYYY'
+        );
+      case 'Hour':
+        return (
+          // HH: ss : AM/PM
+          'HH: ss : AM/PM'
+        );
+      case 'MonthFirst':
+        return (
+          // mm/dd/yyyy
+          'MM/DD/YYYY'
+        );
+      default:
+        return null;
+    }
+  }
+
   render() {
-    const now = moment().format('MM/DD/YYYY');
+    const now = moment().format(this.renderSwitch('Day'));
+
     return (
       <div>
+        <select name="selectFormat">
+          <option value={this.renderSwitch('DayHour')}>Día con hora</option>
+          <option value={this.renderSwitch('Day')}>Día</option>
+          <option value={this.renderSwitch('Hour')}>Hora</option>
+          <option value={this.renderSwitch('MonthFirst')}>Mes inicial</option>
+        </select>
+
         <span>{now}</span>
       </div>
     );
